d9f384ffe8562f5016e694a356266859ab9c9381
LoveCodes <incrediblelove2017@gmail.com>
2019-01-22 18:16:56 +0100

    worked on the project

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 const express = require('express')
-
 const path = require("path")
+const morgan = require("morgan")
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const PORT = 5000
 
+let UserArray = []
+
+app.use(morgan('dev'))
+
+//parses form object from client side and make it available on req.body
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//middleware to serve static files
 app.use(express.static(path.join(__dirname, "./public")))
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/others/login.html"))
-})
+//set up viewing engine
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/others/signup.html"))
+    res.render("login", {
+        pageTitle: "Login"
+    })
 })
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html"))
+app.get('/signup', (req, res) => {
+    res.render("signup", {
+        pageTitle: "Sign Up"
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        pageTitle: "index"
+    })
+})
+
+app.post('submitForm', (req, res) => {
+    let userObject = req.body
+    UserArray.push(userObject)
+    console.log(UserArray)
+    res.redirect('/')
 })
 
 app.listen(PORT,function(){
